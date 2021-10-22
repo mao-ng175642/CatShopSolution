570eadaa26bc57404ed36f23875f41f410e0008e
mao.ng175642@sis.hust.edu.vn <mao.ng175642@sis.hust.edu.vn>
2021-10-22 22:19:31 +0700

    Fluent_Validator

--- CatShopSolution.ViewModels/System/Users/LoginRequestValidator.cs
+++ CatShopSolution.ViewModels/System/Users/LoginRequestValidator.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CatShopSolution.ViewModels.System.Users
 {
-    class LoginRequestValidator
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
     {
+        public LoginRequestValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("User name is required");
+
+            RuleFor(x => x.PassWord).NotEmpty().WithMessage("Password is required")
+                .MinimumLength(6).WithMessage("Password is at least 6 charactor");
+
+        }
     }
 }
